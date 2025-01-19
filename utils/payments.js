3d1be3f8ceeee524bdbe53caa4acfa8946dac6a7
levihoulton <levijhoulton@gmail.com>
2025-01-19 14:14:07 -0600

    updated formatting of date

--- utils/payments.js
+++ utils/payments.js
@@ -1,13 +1,23 @@
 const { pool } = require("../dbConfig"); // Import the pool directly
+const { format } = require('date-fns');
+
 
 // Function to insert payments into the PaymentImport table
 async function insertPaymentsToDatabase(payments) {
     const query = `
-        INSERT INTO enerlux.PaymentImport (
-            PaymentKey, OrderNumber, CustomerRef, PaymentDate, PaymentAmount, PaymentNote, LastUpdatedTime
-        )
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    `;
+    INSERT INTO enerlux.PaymentImport (
+        PaymentKey, OrderNumber, CustomerRef, PaymentDate, PaymentAmount, PaymentNote, LastUpdatedTime
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+        OrderNumber = VALUES(OrderNumber),
+        CustomerRef = VALUES(CustomerRef),
+        PaymentDate = VALUES(PaymentDate),
+        PaymentAmount = VALUES(PaymentAmount),
+        PaymentNote = VALUES(PaymentNote),
+        LastUpdatedTime = VALUES(LastUpdatedTime)
+`;
+
 
     try {
         const connection = await pool.getConnection(); // Get connection from the pool
@@ -42,7 +52,10 @@ async function getLatestPaymentsFromDatabase() {
         const [LastUpdatedTime] = await connection.query(query); // Execute query and get the latest payment
         connection.release(); // Release the connection back to the pool
         console.log(LastUpdatedTime)
-        return LastUpdatedTime[0]?.LastUpdatedTime ?? '2015-01-16'; // Return the latest payment data
+
+        const formattedDate = format(new Date('2024-11-20T03:13:33.000Z'), 'yyyy-MM-dd');
+        
+        return LastUpdatedTime[0]?.LastUpdatedTime ? format(new Date(LastUpdatedTime[0]?.LastUpdatedTime), 'yyyy-MM-dd') : '2015-01-16'; // Return the latest payment data
     } catch (err) {
         console.error('Error fetching latest payment from database:', err);
         throw err; // Throw error to be caught in the route
